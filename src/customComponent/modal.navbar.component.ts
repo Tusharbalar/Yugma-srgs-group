c7b53c103cc44cde89f461e13b7599342830e3bb
akash-gupta-146 <ag.akash007@gmail.com>
2017-02-09 01:19:30 +0530

    Close button: changed size of close button in android side

--- src/customComponent/modal.navbar.component.ts
+++ src/customComponent/modal.navbar.component.ts
@@ -7,8 +7,8 @@ import { Events, ViewController } from 'ionic-angular';
     <ion-toolbar color="primary">
       <ion-buttons start>
         <button ion-button color="light" class="navBtnRight" (click)="dismiss()">
-          <span color="light" showWhen="ios"><ion-icon name="close-circle"></ion-icon></span>
-          <ion-icon color="light" name="md-close" showWhen="android,windows"></ion-icon>
+          <span color="light" showWhen="ios">Cancel</span>
+          <ion-icon color="light" name="md-close" class="csBigIcon" showWhen="android,windows"></ion-icon>
         </button>
       </ion-buttons>
       <ion-title>
